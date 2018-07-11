8a7f48d1ebdfef67c3303e49718d3d36b982b88d
Tommy Susanto <tommy88susanto@gmail.com>
2018-07-11 14:13:16 +1000

    0.0.3 fix problem on windows machines

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
-const child_process =require('child_process')
+const child_process = require('child_process')
 
 const registry = process.argv[2]
 const username = process.argv[3];
 const password = process.argv[4];
 const email = process.argv[5];
 
-const childProcess = child_process.spawn('npm', ['login','--registry',`${registry}`])
+const windowsEnvironment = process.platform === "win32";
+
+if (windowsEnvironment) {
+    var cmd = 'npm.cmd'
+} else {
+    var cmd = 'npm'
+}
+
+const childProcess = child_process.spawn(cmd, ['login', '--registry', `${registry}`]);
+
 childProcess.stdout.on('data', function (data) {
     console.log(data.toString())
-    if (data.toString()=='Username: '){
+    if (data.toString() == 'Username: ') {
         childProcess.stdin.write(`${username}\n`)
     }
     if (data.toString() == 'Password: ') {
@@ -18,9 +27,21 @@ childProcess.stdout.on('data', function (data) {
     if (data.toString() == 'Email: (this IS public) ') {
         childProcess.stdin.write(`${email}\n`)
     }
+
+    setTimeout(() => {
+        try {
+            if (windowsEnvironment) {
+                child_process.exec('taskkill /pid ' + childProcess.pid + ' /T /F')
+            } else {
+                childProcess.kill();
+            }
+        }
+        catch (e) {
+            console.error('can\'t kill process', e);
+        }
+    }, 1000);
 });
 
 childProcess.stderr.on('data', function (error) {
     console.log(error.toString())
 });
-
